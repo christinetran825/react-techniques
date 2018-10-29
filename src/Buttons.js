ad459a66da60bbe1512ca98b90d5a1d5496d6367
github username <github email address>
2018-10-28 19:54:26 -0700

    imported Likes component to button page

---
+++ src/Buttons.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Likes from './Likes.js'
+
+class Buttons extends React.Component {
+
+  render(){
+    return(
+      <div>
+        <Likes/>
+      </div>
+    );
+  }
+
+}
+
+export default Buttons;
