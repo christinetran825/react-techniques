b6123d0e30c2a7f37b9c6564ebe9ed8ee3d2def6
github username <github email address>
2018-10-31 12:46:06 -0700

    added fontawesome icons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './Routes.js';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faBeer, faCoffee, faWineGlassAlt, faStroopwafel, faAppleAlt, faCookie, faCheckSquare as fasCheckSquare} from '@fortawesome/free-solid-svg-icons'
+import { faSquare as farSquare, faCheckSquare as farCheckSquare } from '@fortawesome/free-regular-svg-icons'
+
+library.add(
+  fab,
+  farSquare,
+  faBeer,
+  faCoffee,
+  faWineGlassAlt,
+  faStroopwafel,
+  faAppleAlt,
+  faCookie,
+  fasCheckSquare,
+  farCheckSquare
+)
 
 class App extends Component {
   render() {
