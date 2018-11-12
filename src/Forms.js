7770ce350ec758d27d3ff9bf92ae3340deb98f00
github username <github email address>
2018-11-11 18:39:11 -0800

    added a new section called forms to included CreditCards component

---
+++ src/Forms.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CreditCards from './CreditCards.js';
+
+class Forms extends React.Component {
+
+  render(){
+    return(
+      <section className="forms">
+        <CreditCards/>
+      </section>
+    );
+  }
+
+}
+
+export default Forms;
