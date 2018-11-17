4c441227237dddad1380c7c0fee04c08d5e382c4
github username <github email address>
2018-11-16 18:35:04 -0800

    added shipping code

--- src/CreditCards.js
+++ src/CreditCards.js
@@ -15,6 +15,43 @@ class Likes extends React.Component {
     return(
       <div className="credit-cards">
         <h2>Credit Cards</h2>
+        <h5>Your Cart</h5>
+        <h5>Shipping</h5>
+        <form>
+          <label>Email</label>
+          <input type="text" name="email" />
+          <br></br>
+          <label>Full Name</label>
+          <input type="text" name="name" />
+          <br></br>
+          <label>Country</label>
+          <input type="text" name="country" />
+          <br></br>
+          <label>Address</label>
+          <input type="text" name="address" />
+          <br></br>
+          <label>Apt / Floor / Suite / Other <span> (optional)</span></label>
+          <input type="text" name="address-opt" />
+          <br></br>
+          <label>City</label>
+          <input type="text" name="city" />
+          <br></br>
+          <label>State / Province / Region</label>
+          <input type="text" name="state" />
+          <br></br>
+          <label>Zip / Postal Code</label>
+          <input type="text" name="zip" />
+          <br></br>
+          <label>Phone <span>(in case of delivery issues)</span></label>
+          <input type="number" name="phone" />
+          <br></br>
+          <label>Special Delivery Instructions <span>(optional)</span></label>
+          <input type="text" name="delivery-message" />
+          <br></br>
+          <p>Save Shipping Address</p>
+        </form>
+        <h5>Billing</h5>
+        <h5>Payment Methods</h5>
         <div className="pay-methods">
           <FontAwesomeIcon icon={['far', 'credit-card']} />
           <FontAwesomeIcon icon={['fab', 'cc-paypal']} />
